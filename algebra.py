432ed6e338ed8018dc8a1db69c496217be068c2d
Jose G. Castillo <Gus@Mac-Owner-1s-MacBook-Pro.local>
2019-01-08 01:36:53 -0500

    quad_form function now elimates duplicate values of x and removes imaginary numbers if they are equivalent to zero

--- algebra.py
+++ algebra.py
@@ -9,8 +9,25 @@ def quad_form(coef_a, coef_b, coef_c):
 	x_add =(-coef_b+sqrt_numerator)/denominator
 	x_sub =(-coef_b-sqrt_numerator)/denominator
 
-	x_values = [x_add, x_sub]
+	unorg_values = [x_add, x_sub]
+	for unorg_value in unorg_values:
+		if unorg_value.imag == 0.0:
+			x_values = [x_add.real, x_sub.real]
+		else:
+			x_values = [x_add, x_sub]
+
 	x_values = np.sort(x_values)
-	x_values = tuple(x_values)
+	x_values = set(x_values)
+	
+	if len(x_values) > 1:
+
+		x_values = tuple(x_values)
+	else:
+		x_values = x_values.pop()
 	
 	return x_values
+
+#print(quad_form(1,3,-4))
+#print(quad_form(2,-4,-3))
+#print(quad_form(3, 4, 3))
+#print(quad_form(9,12,4))
